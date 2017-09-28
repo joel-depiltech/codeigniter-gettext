8ce1d74654643552c3590d48fc88d3f925502a1f
Joël Gaujard <j.gaujard@gmail.com>
2017-09-29 00:28:41 +0200

    Create a fake function for config_item helper

--- tests/include/gettext_test_functions.php
+++ tests/include/gettext_test_functions.php
@@ -1,9 +1,22 @@
 <?php
 
-// Simulate helper function
+/**
+ * Simulate helper functions
+ */
+
+// fake log message function
 if (!function_exists('log_message')) {
     function log_message($level, $message)
     {
         echo "\n\r" . $level . '|' . $message;
+    }
+}
+
+// fake get config item function
+if (!function_exists('config_item')) {
+    function config_item($key)
+    {
+        require(realpath(__DIR__ . '/../../') . '/src/config/gettext.php');
+        return $config[$key];
     }
 }
