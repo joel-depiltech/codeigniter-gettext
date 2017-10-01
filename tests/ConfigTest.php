5aca766e02a21ac19c3954f848c603b6ba062876
Joël Gaujard <j.gaujard@gmail.com>
2017-10-01 18:17:07 +0200

    Create uit test only for config file

---
+++ tests/ConfigTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace CodeIgniterGetText\Tests;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testCatalogCodeSet()
+    {
+        $config = config_item('gettext_catalog_codeset');
+        $this->assertFalse(empty($config));
+        $this->assertTrue(is_string($config));
+        $this->assertContains($config, mb_list_encodings());
+    }
+
+    public function testTextDomain()
+    {
+        $config = config_item('gettext_text_domain');
+        $this->assertFalse(empty($config));
+        $this->assertTrue(is_string($config));
+    }
+
+    public function testLocaleDir()
+    {
+        $config = config_item('gettext_locale_dir');
+        $this->assertFalse(empty($config));
+        $this->assertTrue(is_string($config));
+        $this->assertTrue(is_dir(APPPATH . $config));
+    }
+
+    public function testLocale()
+    {
+        $config = config_item('gettext_locale');
+        $this->assertFalse(empty($config));
+        $this->assertTrue(is_array($config));
+    }
+
+    public function testCategory()
+    {
+        $config = config_item('gettext_category');
+        $this->assertTrue(is_int($config));
+    }
+
+}
--- tests/include/gettext_test_functions.php
+++ tests/include/gettext_test_functions.php
@@ -16,6 +16,7 @@ if (!function_exists('log_message')) {
 if (!function_exists('config_item')) {
     function config_item($key)
     {
+        $config = array(); // initialize an empty array for avoid IDE error
         require(realpath(__DIR__ . '/../../') . '/src/config/gettext.php');
         return $config[$key];
     }
