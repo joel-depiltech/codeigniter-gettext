b131569b157ec42940b7df8a17df69e1e6ef0fb7
Joël Gaujard <j.gaujard@gmail.com>
2017-10-01 18:31:35 +0200

    Enhance test on helper

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -5,23 +5,40 @@ class HelperTest extends \PHPUnit_Framework_TestCase
 {
     const EXPRESSION = 'Let me test this expression';
     const EXPRESSION_PLURAL = 'Let me test this plural expression';
+    const DOMAIN = 'domain';
+    const CATEGORY = LC_MESSAGES;
 
     public function testDoubleUnderscore()
     {
         $this->assertTrue(function_exists('__'), "__ function does not exists.");
         $this->assertEquals(self::EXPRESSION, __(self::EXPRESSION));
+        $this->assertEquals(self::EXPRESSION, __(self::EXPRESSION, self::DOMAIN));
+        $this->assertEquals(self::EXPRESSION, __(self::EXPRESSION, self::DOMAIN, self::CATEGORY));
     }
 
     public function testUnderscoreE()
     {
         $this->assertTrue(function_exists('_e'), "_e function does not exists.");
-        $this->expectOutputRegex('/' . self::EXPRESSION . '/');
+        $this->expectOutputString(self::EXPRESSION);
+        _e(self::EXPRESSION);
+    }
+
+    public function testUnderscoreE_WithDomain()
+    {
+        $this->expectOutputString(self::EXPRESSION, self::DOMAIN);
+        _e(self::EXPRESSION);
+    }
+
+    public function testUnderscoreE_WithDomainAndCategory()
+    {
+        $this->expectOutputString(self::EXPRESSION, self::DOMAIN, self::CATEGORY);
         _e(self::EXPRESSION);
     }
 
     public function testUnderscoreN()
     {
         $this->assertTrue(function_exists('_n'), "_n function does not exists.");
+
         $this->assertEquals(
             self::EXPRESSION,
             _n(self::EXPRESSION, self::EXPRESSION_PLURAL, 1)
@@ -29,6 +46,24 @@ class HelperTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(
             self::EXPRESSION_PLURAL,
             _n(self::EXPRESSION, self::EXPRESSION_PLURAL, 2)
+        );
+
+        $this->assertEquals(
+            self::EXPRESSION,
+            _n(self::EXPRESSION, self::EXPRESSION_PLURAL, 1, self::DOMAIN)
+        );
+        $this->assertEquals(
+            self::EXPRESSION_PLURAL,
+            _n(self::EXPRESSION, self::EXPRESSION_PLURAL, 2, self::DOMAIN)
+        );
+
+        $this->assertEquals(
+            self::EXPRESSION,
+            _n(self::EXPRESSION, self::EXPRESSION_PLURAL, 1, self::DOMAIN, self::CATEGORY)
+        );
+        $this->assertEquals(
+            self::EXPRESSION_PLURAL,
+            _n(self::EXPRESSION, self::EXPRESSION_PLURAL, 2, self::DOMAIN, self::CATEGORY)
         );
     }
 
